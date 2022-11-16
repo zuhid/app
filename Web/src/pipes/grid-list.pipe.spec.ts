6f554d857b91bd96488d92a53606e2b2a6dd3872
Tanvir Ather <tanvir.ather@flexential.com>
2022-11-16 10:52:54 -0700

    add missing spec

---
+++ Web/src/pipes/grid-list.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GridListPipe } from "./grid-list.pipe";
+
+describe("GridListPipe", () => {
+  it("create an instance", () => {
+    const pipe = new GridListPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
